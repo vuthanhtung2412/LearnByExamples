fdfd8934e7fffff182047af4f3ceada9396b895a
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-12-06 00:34:52 +0100

    run in subproc or same proc

---
+++ bash/source_vs_bash/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# echo $$
+# test1 run source test.sh or . test.sh
+# test2 run bash test.sh or ./test.sh
+
+echo "Sub script PID: $$"
+
+# Change directory to demonstrate the impact
+cd /tmp
