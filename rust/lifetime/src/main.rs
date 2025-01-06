53f36ca0b36c9faedb9b3e8d62a354b29cf65f9f
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2025-01-06 01:33:38 +0100

    minor change

--- rust/lifetime/src/main.rs
+++ rust/lifetime/src/main.rs
@@ -1,5 +1,20 @@
 fn main() {
-    let mut a: &str = "aa";
+    let mut a: &str = &String::from("aa");
+    // Normally ths line above wouldn't work because String::from() create a String internally and
+    // return ownership
+    // However, the `let` construct allow the life time to be extended as long as the liveness of the scompe
+    // The one below breaks 
+    // a  = &String::from("aaaa");
+ 
+    // But this doesn't
+    // let mut a : &str = &String::from("aaaa");
+
+    // but the one below works
+    // TODO: my guess is shadowing bypass borrow checker
+    // let mut a: &str = &String::from("aa");
+    // a  = &String::from("aaaa");
+    // let mut a : &str = &String::from("aaaa");
+
     let mut c: &str;
 
     // This will cause an error since num doesn't live long enough
@@ -7,12 +22,12 @@ fn main() {
     // let num_pointer: &'static i32 = &num;
 
     {
-        // this still works because the lifetime of "ccc" is static
-        let b: &str = "ccc";
+        // this still works because the lifetime of "bbb" is static
+        let b: &str = "bbb";
 
         // the one below will fail
         // because *c get deallocated as soon as the scope end
-        // let s = String::from("ccc");
+        // let s = String::from("bbb");
         // The binding below also determine the lifetime `b,
         // which is the same as the underlying value which ends when deallocation happen (end of the scope)
         // let b: &str = &s; 
@@ -22,7 +37,7 @@ fn main() {
         c = b;                   // ---------+-- 'c = `b
         println!("{}", c);       //          |
         c = longest(a, b); //          +-- 'c = `b U 'a
-    }                            //          |
+    }                            //          +-- the liveness continue since the current value of c is used in the `longest()` function below
                                  //          |
     a = "aaaa";                  //          |
     c = longest(c, a);     //          +-- 'c = `c U 'a = `b U 'a U 'c = `b U 'a
